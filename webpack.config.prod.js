9cb89435fbcb027a606d58be1bead062c49a284a
Alexey Panfilkin <a.panfilkin@oblakogroup.ru>
2018-01-09 16:50:29 +0300

    Webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,17 @@
+plugins: [
+    new webpack.DefinePlugin({ 
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({   <-- Add this
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
